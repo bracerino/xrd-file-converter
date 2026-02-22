c6c1d5bb6880dad1c0af4293e5552fa2a0f50258
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2026-02-22 13:29:21 +0100

    Update xrd_conversion.py

--- xrd_conversion.py
+++ xrd_conversion.py
@@ -710,6 +710,8 @@ def run_axis_converter():
             yaxis_title=y_axis_title,
             height=600,
             hovermode='x unified',
+            font=dict(size=18),   
+            title_font=dict(size=20),
             legend=dict(
                 orientation="h",
                 yanchor="bottom",
