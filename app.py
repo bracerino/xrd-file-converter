c1ae679b7492c307d882bd6f45b03eb4c30e65d0
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2026-03-17 09:18:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1407,7 +1407,7 @@ def run_data_converter():
     with st.expander(f"How to **Cite**", icon="📚", expanded=False):
         st.markdown("""
         If you like the app, please cite the following source:
-        - **XRDlicious, 2025** – [Lebeda, Miroslav, et al. XRDlicious: an interactive web-based platform for online calculation of diffraction patterns and radial distribution functions from crystal structures. Applied Crystallography, 2025, 58.5.](https://doi.org/10.1107/S1600576725005370).
+        - **XRDlicious, 2025** – [Lebeda, Miroslav, et al. XRDlicious: an interactive web-based platform for online calculation of diffraction patterns and radial distribution functions from crystal structures. Applied Crystallography, 2025, 58.5.](https://journals.iucr.org/j/issues/2025/05/00/hat5006/index.html).
         """)
     #st.markdown(
     #    """
